9a768ea2d764ca14e5a0fc7948ad6e31f0fe6a69
Ravindra Paswan <69711807+ravindrapaswan2762@users.noreply.github.com>
2023-08-25 13:59:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let arr = [];
 
 // Function to update the clock time every second
 function updateClock() {
-  const now = new Date();  
+  const now = new Date(); // Get the current date and time
   const timeElement = document.getElementById('time'); // Get the element to display time
   timeElement.innerText = now.toLocaleTimeString(); // Update the displayed time
 }
@@ -14,6 +14,7 @@ function updateClock() {
 
 // Function to set an alarm
 function setAlarm() {
+  // Get values from input fields for the alarm hour, minute, second, and AM/PM selection
   const alarmHour = parseInt(document.getElementById('alarmHour').value);
   const alarmMinute = parseInt(document.getElementById('alarmMinute').value);
   const alarmSecond = parseInt(document.getElementById('alarmSecond').value);
@@ -75,6 +76,39 @@ function deleteAlarm(alarmItem) {
   }
 }
 
+// ----------------------------------------------
+
+// This function plays a notification sound for the given alarmItem and shows a confirmation dialog.
+function playNotificationSound(alarmItem) {
+  
+  var sound = document.getElementById('notificationSound');// Get the audio element with the ID 'notificationSound'
+  
+  sound.loop = true; // Set the audio to loop continuously
+  
+  // If the sound is paused, reset its current time to the beginning
+  if (sound.paused) {
+    sound.currentTime = 0;
+  }
+  
+  sound.play(); // Play the notification sound
+
+  // Set up an interval to display the confirmation dialog every 1 second
+  var dialogInterval = setInterval(function() {
+    // Show a confirmation dialog with a message
+    var dialogResult = confirm('Alarm is going off! Do you want to stop the alarm?');
+
+    // If the user confirms (clicks 'OK') on the dialog
+    if (dialogResult) {
+
+      sound.pause(); // Pause the notification sound
+
+      deleteAlarm(alarmItem); // Delete the alarm to prevent it from alerting again
+
+      clearInterval(dialogInterval); // Clear the interval to stop showing the confirmation dialog
+    }
+  }, 1000); // Check the dialog every 1 second
+}
+
 //-----------------------------------------------
 
 // Function to check alarms and show alerts when an alarm goes off
@@ -85,8 +119,7 @@ function checkAlarms() {
   //Match the current time from arr list items.
   for (let i = 0; i < arr.length; i++) {
     if (now == arr[i].innerText.substring(0, 10)) {
-      alert('Alarm is going off!'); // Show an alert if the alarm time matches the current time
-      deleteAlarm(arr[i]); // Delete the alarm so it won't alert again
+      playNotificationSound(arr[i]);
       arr.splice(i, 1); // Remove the alarm time from the array
       i--;
     }
